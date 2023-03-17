45a8d514bb1b45ff6fe619c28f837e48eb837f74
norskemarcus <marc791j@stud.kea.dk>
2023-03-17 14:12:07 +0100

    init commit

---
+++ src/test/java/dk/kea/project1backend/Project1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.project1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
