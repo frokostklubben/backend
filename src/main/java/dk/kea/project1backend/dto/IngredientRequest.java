52043465edcc3eec9eb3fc65371db9d4888ec6c5
Tomas Martins <toma3026@stud.kea.dk>
2023-03-22 09:16:51 +0100

    full crud for fridge

--- src/main/java/dk/kea/project1backend/dto/IngredientRequest.java
+++ src/main/java/dk/kea/project1backend/dto/IngredientRequest.java
@@ -1,10 +1,14 @@
 package dk.kea.project1backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class IngredientRequest {
 
     private Integer id;
